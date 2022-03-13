987bb7ea9c585018cae17ce1e36ee338619220f3
Asaedry <marshallchrismiller@gmail.com>
2022-03-12 20:49:27 -0800

    fix js images locations

--- Resources/javascript/catBio.js
+++ Resources/javascript/catBio.js
@@ -10,7 +10,7 @@ class Example extends Phaser.Scene {
     }
 
     preload() {
-        this.load.spritesheet('cat', '/Resources/imgs/cats/CatSpriteSheet.png', { frameWidth: 32, frameHeight: 32 });
+        this.load.spritesheet('cat', '../Resources/imgs/cats/CatSpriteSheet.png', { frameWidth: 32, frameHeight: 32 });
     }
 
     create () {
